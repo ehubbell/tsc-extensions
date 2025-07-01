d6c856a12aecdf34de934e7dd98b5b5ba58add20
playbooks <eric@erichubbell.com>
2025-07-01 08:26:21 -0600

    config updates

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,35 @@ import fs from 'fs-extra';
 import path from 'path';
 
 async function formatExtensions(pathName) {
-	const fileContent = await fs.promises.readFile(pathName, 'utf8');
+	const originalContent = await fs.promises.readFile(pathName, 'utf8');
 
-	const importRegex = /(import\s+.*?from\s+['"])([^'"]+)(['"])/g;
+	const exportRegex = /(export\s+.*?from\s)/g;
 
-	const modifiedContent = fileContent.replace(importRegex, (match, beforePath, importPath, afterPath) => {
-		if (importPath.startsWith('./') || importPath.startsWith('../')) {
-			const hasExtension = path.extname(importPath) !== '';
-			if (!hasExtension) return `${beforePath}${importPath}.d.ts${afterPath}`;
+	const modifiedExport = originalContent.replace(exportRegex, (match, beforePath, matchedPath, afterPath) => {
+		if (matchedPath) return `export type * from `;
+		return match;
+	});
+
+	await fs.promises.writeFile(pathName, modifiedExport, 'utf8');
+
+	const updatedContent = await fs.promises.readFile(pathName, 'utf8');
+
+	const pathRegex = /(export\s+.*?from\s+['"])([^'"]+)(['"])/g;
+
+	const modifiedPaths = updatedContent.replace(pathRegex, (match, beforePath, matchedPath, afterPath) => {
+		if (matchedPath.startsWith('./') || matchedPath.startsWith('../')) {
+			const hasExtension = path.extname(matchedPath) !== '';
+			if (!hasExtension) return `${beforePath}${matchedPath}.d.ts${afterPath}`;
 		}
 		return match;
 	});
 
-	await fs.promises.writeFile(pathName, modifiedContent, 'utf8');
+	await fs.promises.writeFile(pathName, modifiedPaths, 'utf8');
 }
 
 export const runExtensions = (pathName = 'dist/index.d.ts'): any => {
 	return {
-		name: 'run-size',
+		name: 'run-extensions',
 		closeBundle: async () => {
 			await formatExtensions(pathName);
 			console.log(`${pathName} updated!`);
