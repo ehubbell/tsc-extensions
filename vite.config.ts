9a9642c3c5ce07bdf0f9d2d67fc8947fe3e1bce3
playbooks <eric@erichubbell.com>
2025-07-01 07:26:24 -0600

    config updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import path from 'path';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import { defineConfig } from 'vite';
 
 export default defineConfig(({ mode }) => {
@@ -8,9 +9,24 @@ export default defineConfig(({ mode }) => {
 			sourcemap: mode !== 'production',
 			lib: {
 				entry: path.resolve(__dirname, 'src/index.ts'),
-				name: 'mylib',
-				formats: ['es', 'cjs', 'umd', 'iife'],
-				fileName: format => `index.${format}.js`,
+				name: 'Extensions',
+				formats: ['es', 'cjs'],
+				fileName: (format, entryName) => `${entryName}.${format}.js`,
+			},
+			rollupOptions: {
+				external: [
+					'node:fs',
+					'node:stream',
+					'node:zlib',
+					'node:util',
+					'constants',
+					'path',
+					'stream',
+					'fs',
+					'util',
+					'assert',
+				],
+				plugins: [peerDepsExternal()],
 			},
 		},
 		plugins: [],
